c3d24b094e0e51859c023226ee6cfde4df531d25
FAWC-bupt <Kevin_lgh@outlook.com>
2021-01-01 16:31:11 +0800

    创建项目

---
+++ src/main/java/bupt/weibo/WeiboApplication.java
@@ -0,0 +1,13 @@
+package bupt.weibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeiboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeiboApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bupt/weibo/WeiboApplicationTests.java
@@ -0,0 +1,13 @@
+package bupt.weibo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeiboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
